cfc5807281b054616eb58dd80bb8537e82a48aaf
gokul <gopalakrishnan93843@gmail.com>
2025-10-29 15:41:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,43 +1,17 @@
-# variables.tf
-
-variable "region" {
-  description = "AWS Region"
+variable "aws_region" {
+  description = "AWS region to deploy the infrastructure"
   type        = string
   default     = "ap-south-1"
 }
 
-variable "ami" {
-  description = "AMI ID for EC2"
-  type        = string
-  default     = "ami-0f9708d1cd2cfee41"  # Ubuntu 22.04
-}
-
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Type of EC2 instance"
   type        = string
   default     = "t2.micro"
 }
 
-variable "vpc_cidr" {
-  description = "VPC CIDR block"
+variable "vpc_name" {
+  description = "Name of the VPC"
   type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_cidr" {
-  description = "Subnet CIDR block"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "ssh_port" {
-  description = "SSH port"
-  type        = number
-  default     = 22
-}
-
-variable "http_port" {
-  description = "HTTP port"
-  type        = number
-  default     = 80
+  default     = "my-vpc"
 }
